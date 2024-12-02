4af849eeefed9e59936ac09ef33bb88ed488c776
Souza <souzarodrigo0610@gmail.com>
2024-12-02 18:25:00 -0300

    Refazendo o aoi.music

---
+++ types/index.d.ts
@@ -0,0 +1,3 @@
+import { GuildQueueEvent, QueueRepeatMode } from "discord-player";
+import { Manager } from "./classes/structures/aoiMusic";
+export { ForgeMusic, GuildQueueEvent, QueueRepeatMode };
